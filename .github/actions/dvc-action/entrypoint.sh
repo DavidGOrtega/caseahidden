e3d39b8f71a831e40be325bce7395f8eed718996
davidgortega <g.ortega.david@gmail.com>
2019-12-18 14:41:10 +0100

    full action

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -8,13 +8,13 @@ dvc pull
 echo DVC repro ${dvc_file}
 dvc repro ${dvc_file}
 
-# if ! git diff-index --quiet HEAD --; then
-#     echo dvc updated the repo, pushing...
+if ! git diff-index --quiet HEAD --; then
+    echo dvc updated the repo, pushing...
 
-#     git add --all
-#     git commit -m "dvc repro [skip ci]"
-#     git push
-#     dvc push
+    git add --all
+    git commit -m "dvc repro [skip ci]"
+    git push
+    dvc push
 
-#     echo "done!"
-# fi
+    echo "done!"
+fi
