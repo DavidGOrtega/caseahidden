3fda2b7624c94575d236b94da8efbac0e03e4e38
davidgortega <g.ortega.david@gmail.com>
2019-12-18 18:04:34 +0100

    no echoes

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -7,11 +7,7 @@ COMMIT_FILTER="dvc repro"
 
 # Check skip ci
 readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
-echo "Last commit log: $last_commit_log"
-
 readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
-echo "Number of occurences of '$COMMIT_FILTER' in '$last_commit_log': $filter_count"
-
 if ! [[ "$filter_count" -eq 0 ]]; then
   echo "Last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", stopping"
   exit 0 # exit 78
