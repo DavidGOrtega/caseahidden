b9c1e6256677159fa79fdbd48b357516b9b56990
DavidGOrtega <g.ortega.david@gmail.com>
2019-12-19 18:11:06 +0100

    4 (#7)
    * 4
    
    * dvc repro; Leave me to ci skip!

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import json
 import time
 
 start = time.time()
-time. sleep(3)
+time. sleep(4)
 
 with open('train.json', 'w') as outfile:
     json.dump({ "took" : (time.time() - start) }, outfile)
