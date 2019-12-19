9bfcb6a9d55be5ed790455f0441c7981fab008bd
davidgortega <g.ortega.david@gmail.com>
2019-12-19 01:23:33 +0100

    4

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -27,11 +27,17 @@ dvc repro ${dvc_file}
 
 if ! git diff-index --quiet HEAD --; then
     echo Pushing to repo
-    git config --local user.email "action@github.com"
-    git config --local user.name "GitHub Action"
-    # git config --local user.name "${GITHUB_ACTOR}"
+    
+    # git config --local user.email "action@github.com"
+    # git config --local user.name "GitHub Action"
+    # git commit -m "${COMMIT_FILTER}" -a
+    # git push "${remote_repo}" HEAD:${branch}
+
+    git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+    git pull github ${GITHUB_REF} --ff-only
+
     git commit -m "${COMMIT_FILTER}" -a
-    git push "${remote_repo}" HEAD:${branch}
+    git push github HEAD:${GITHUB_REF}
 
     echo Pushing to dvc repo
     #dvc push
