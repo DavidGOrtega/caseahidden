e4dca2e46d2e50f30f7f97c18d294c1e7b267584
davidgortega <g.ortega.david@gmail.com>
2019-12-19 02:35:16 +0100

    fix skip

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -5,13 +5,13 @@ set -e
 
 COMMIT_FILTER="dvc repro"
 # Skip if commit filter
-last_commit_log=$(git log -1 --pretty=format:"%s")
+readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
+readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
 echo "Check skip last commit $last_commit_log"
-filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
 if ! [[ "$filter_count" -eq 0 ]]; then
   echo "Last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", skipping"
   exit 0 # exit 78 # 78 is neutral github code 
-fi
+fiz
 
 dvc_file=${dvc_file:-Dvcfile}
 echo Pulling from dvc repo...
