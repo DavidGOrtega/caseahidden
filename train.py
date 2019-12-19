764319a3b5533e051d81f3419f91007b7f72c5a9
davidgortega <g.ortega.david@gmail.com>
2019-12-19 02:21:50 +0100

    5

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import json
 import time
 
 start = time.time()
-time. sleep(4)
+time. sleep(5)
 
 with open('train.json', 'w') as outfile:
     json.dump({ "took" : (time.time() - start) }, outfile)
