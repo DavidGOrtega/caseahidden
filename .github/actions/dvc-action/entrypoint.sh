48ba5713a6e600a04f1553dfdc16c680192edd14
davidgortega <g.ortega.david@gmail.com>
2019-12-19 02:30:18 +0100

    HERE

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -5,8 +5,9 @@ set -e
 
 COMMIT_FILTER="dvc repro"
 # Skip if commit filter
-readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
-readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
+echo "Check skip"
+last_commit_log=$(git log -1 --pretty=format:"%s")
+filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
 if ! [[ "$filter_count" -eq 0 ]]; then
   echo "Last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", skipping"
   exit 0 # exit 78 # 78 is neutral github code 
