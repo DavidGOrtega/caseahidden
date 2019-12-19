608463599816f9a8a6ef1a13f1646d69844192bf
davidgortega <g.ortega.david@gmail.com>
2019-12-19 01:50:03 +0100

    4

--- .github/actions/dvc-action/entrypoint.sh
+++ .github/actions/dvc-action/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 set -e
-
 printenv
 
 COMMIT_FILTER="dvc repro"
-
 # Skip if commit filter
 # readonly local last_commit_log=$(git log -1 --pretty=format:"%s")
 # readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
@@ -24,21 +22,15 @@ dvc repro ${dvc_file}
 if ! git diff-index --quiet HEAD --; then
     echo Pushing to repo
     
-    # remote_repo="https://${GITHUB_ACTOR}:${github_token}@github.com/$GITHUB_REPOSITORY.git"
-    # branch=${GITHUB_REF#refs/heads/}
-    # git config --local user.email "action@github.com"
-    # git config --local user.name "GitHub Action"
-    # git commit -m "${COMMIT_FILTER}" -a
-    # git push "${remote_repo}" HEAD:${branch}
-
-    
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
     git commit -m "${COMMIT_FILTER}" -a
     git remote add github "https://$GITHUB_ACTOR:$github_token@github.com/$GITHUB_REPOSITORY.git"
     git push github HEAD:${GITHUB_REF}
 
-    echo Pushing to dvc repo
-    #dvc push
+    if [ "${dvc_push:-true}" = true ] ; then
+        echo Pushing to dvc repo
+        dvc push
+    fi
 fi
 
