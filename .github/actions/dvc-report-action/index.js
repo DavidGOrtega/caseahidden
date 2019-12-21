9560e9d1330610cc31bbb13bf0cbe68bd7452cce
davidgortega <g.ortega.david@gmail.com>
2019-12-21 14:07:29 +0100

    trial

--- .github/actions/dvc-report-action/index.js
+++ .github/actions/dvc-report-action/index.js
@@ -1,48 +1,64 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const myToken = core.getInput('github_token');
+async function checks() {
+  try {
+    const myToken = core.getInput('github_token');
+  
+    const octokit = new github.GitHub(myToken);
+    const owner = 'DavidGOrtega';
+    const repo = 'caseahidden';
+    const head_sha = github.context.sha;
 
-  const octokit = new github.GitHub(myToken);
-  octokit.checks.create({
-    owner: 'DavidGOrtega',
-    repo: 'caseahidden',
-    head_sha: github.context.sha,
+    await octokit.checks.create({
+      owner,
+      repo,
+      head_sha,
+  
+      started_at: new Date(),
+      completed_at: new Date(),
+      conclusion: 'success',
+  
+      name: 'DVC Report',
+      status: 'completed',
+      output: {
+        title: 'Checksum test',
+        summary: 'skjdsjdskdskdjs',
+      }
+    })
+  
+    console.log(github);
+    console.log(github.context);
+    console.log(github.context.payload);
+    const repo_checks = await octokit.checks.listForRef({
+      owner,
+      repo,
+      head_sha,
+    });
 
-    started_at: new Date(),
-    completed_at: new Date(),
-    conclusion: 'success',
+    console.log(repo_checks);
 
-    name: 'DVC test',
-    status: 'completed',
-    output: {
-      title: 'Checksum test',
-      summary: 'skjdsjdskdskdjs',
-    }
-  })
-
-  console.log(github);
-  console.log(github.context);
-  console.log(github.context.payload);
-  /* octokit.checks.update({
-    check_run_id: ,
-    owner: 'DavidGOrtega',
-    repo: 'caseahidden',
-    head_sha: github.context.sha,
-
-    started_at: new Date(),
-    completed_at: new Date(),
-    conclusion: 'success',
-
-    name: 'DVC test',
-    status: 'completed',
-    output: {
-      title: 'Checksum test',
-      summary: 'skjdsjdskdskdjs',
-    }
-  }) */
-
-} catch (error) {
-  core.setFailed(error.message);
+    /* octokit.checks.update({
+      check_run_id: ,
+      owner: 'DavidGOrtega',
+      repo: 'caseahidden',
+      head_sha: github.context.sha,
+  
+      started_at: new Date(),
+      completed_at: new Date(),
+      conclusion: 'success',
+  
+      name: 'DVC test',
+      status: 'completed',
+      output: {
+        title: 'Checksum test',
+        summary: 'skjdsjdskdskdjs',
+      }
+    }) */
+  
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+checks();
