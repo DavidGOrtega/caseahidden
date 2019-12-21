98b55159e76bfb288c776b59d21007c7c5804901
davidgortega <g.ortega.david@gmail.com>
2019-12-21 13:41:25 +0100

    trial

--- .github/actions/dvc-report-action/index.js
+++ .github/actions/dvc-report-action/index.js
@@ -2,31 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  console.log(github);
   const myToken = core.getInput('who-to-greet');
 
   const octokit = new github.GitHub(myToken);
   octokit.checks.create({
     owner: 'DavidGOrtega',
-    repo:'caseahidden',
+    repo: 'caseahidden',
     head_sha: github.context.sha,
 
     started_at: new Date(),
     completed_at: new Date(),
     conclusion: 'success',
 
-    name: 'DVC test',
+    name: 'run',
     status: 'completed',
     output: {
       title: 'Checksum test',
