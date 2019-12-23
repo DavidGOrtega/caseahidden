69d9255e98594a11df186c5b30b81822fb8f02c8
davidgortega <g.ortega.david@gmail.com>
2019-12-24 00:44:19 +0100

    s

--- .github/actions/dvc-report-action/index.js
+++ .github/actions/dvc-report-action/index.js
@@ -62,80 +62,80 @@ const data_report_summary_md = async () => {
 
 const check_dvc_data_report = async () => {
 
-  try {
-    const started_at = new Date();
-    const name = 'DVC Data Report';
-    const conclusion = 'success';
-    const title = 'Checksum Test';
-    const summary = await data_report_summary_md();
+  const started_at = new Date();
+  const name = 'DVC Data Report';
+  const conclusion = 'success';
+  const title = 'Checksum Test';
+  const summary = await data_report_summary_md();
 
-    await octokit.checks.create({
-      owner,
-      repo,
-      GITHUB_SHA,
+  await octokit.checks.create({
+    owner,
+    repo,
+    GITHUB_SHA,
 
-      started_at,
-      name,
-      conclusion,
-      completed_at: new Date(),
-      status: 'completed',
-      output: {
-        title,
-        summary
-      }
-    })
-  
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    started_at,
+    name,
+    conclusion,
+    completed_at: new Date(),
+    status: 'completed',
+    output: {
+      title,
+      summary
+    }
+  })
 }
 
 
 const run_action = async () => {
 
-  console.log('Checking skip');
-  const last_log = await exe('$(git log -1');
-  if (last_log.includes(skip_ci)) {
-    console.log(`${skip_ci} found! skipping task`);
-    return 0;
-  }
+  try {
+    console.log('Checking skip');
+    const last_log = await exe('git log -1');
+    if (last_log.includes(skip_ci)) {
+      console.log(`${skip_ci} found! skipping task`);
+      return 0;
+    }
 
-  console.log('Pulling from dvc remote');
-  await exe('dvc pull');
+    console.log('Pulling from dvc remote');
+    await exe('dvc pull');
 
-  check_dvc_data_report();
+    check_dvc_data_report();
 
-  const dvc_repro_file_exists = fs.existsSync(dvc_repro_file);
-  if (!dvc_repro_skip && dvc_repro_file_exists) {
+    const dvc_repro_file_exists = fs.existsSync(dvc_repro_file);
+    if (!dvc_repro_skip && dvc_repro_file_exists) {
 
-    console.log(`echo Running dvc repro ${dvc_repro_file}`);
-    await exe(`dvc repro ${dvc_repro_file}`);
+      console.log(`echo Running dvc repro ${dvc_repro_file}`);
+      await exe(`dvc repro ${dvc_repro_file}`);
 
-    const has_changes = false; //if ! git diff-index --quiet HEAD --; then
-    if (has_changes) {
-      console.log('Pushing to repo');
+      const has_changes = false; //if ! git diff-index --quiet HEAD --; then
+      if (has_changes) {
+        console.log('Pushing to repo');
 
-      await exe(`
-        dvc commit -f && \
-        git config --local user.email "action@github.com" && \
-        git config --local user.name "GitHub Action" && \
-        git commit -m "dvc repro ${skip_ci}" -a && \
-        git remote add github "https://${GITHUB_ACTOR}:${github_token}@github.com/${GITHUB_REPOSITORY}.git"
-        git push github HEAD:${GITHUB_REF}
-      `);
+        await exe(`
+          dvc commit -f && \
+          git config --local user.email "action@github.com" && \
+          git config --local user.name "GitHub Action" && \
+          git commit -m "dvc repro ${skip_ci}" -a && \
+          git remote add github "https://${GITHUB_ACTOR}:${github_token}@github.com/${GITHUB_REPOSITORY}.git"
+          git push github HEAD:${GITHUB_REF}
+        `);
 
-      if (dvc_push) {
-        console.log('Pushing to dvc remote');
-        await exe('dvc push');
+        if (dvc_push) {
+          console.log('Pushing to dvc remote');
+          await exe('dvc push');
+        }
+
+      }
+      
+      
+      if (!dvc_repro_skip && !dvc_repro_file_exists) {
+        console.log(`DVC repro file ${dvc_repro_file} not found`);
       }
 
     }
-    
-    
-    if (!dvc_repro_skip && !dvc_repro_file_exists) {
-      console.log(`DVC repro file ${dvc_repro_file} not found`);
-    }
-
+  
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
 
